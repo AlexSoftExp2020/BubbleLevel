9ff988d20429074c78a5492c3772598ab6983be0
Alex <oleksii.pidhornyi@gmail.com>
2025-01-23 22:16:48 +0100

    Initial Commit

---
+++ BubbleLevel/BubbleLevelApp.swift
@@ -0,0 +1,17 @@
+//
+//  BubbleLevelApp.swift
+//  BubbleLevel
+//
+//  Created by Oleksii on 23.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct BubbleLevelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
