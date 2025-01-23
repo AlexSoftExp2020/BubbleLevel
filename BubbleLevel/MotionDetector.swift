f93a0c376d77154efb2f7cee6cc2562b5b2c913e
Alex <oleksii.pidhornyi@gmail.com>
2025-01-23 22:41:03 +0100

    add motion detector

---
+++ BubbleLevel/MotionDetector.swift
@@ -0,0 +1,50 @@
+//
+//  MotionDetector.swift
+//  BubbleLevel
+//
+//  Created by Oleksii on 23.01.2025.
+//
+
+import CoreMotion
+import UIKit
+
+class MotionDetector: ObservableObject {
+    private let motionManager = CMMotionManager()
+    
+    private var timer = Timer()
+    private var updateInterval: TimeInterval
+    
+    @Published var pitch: Double = 0
+    @Published var roll: Double = 0
+    @Published var zAcceleration: Double = 0
+    
+    var onUpdate: (() -> Void) = {}
+    
+    private var currentOrientation: UIDeviceOrientation = .landscapeLeft
+    private var orientationObserver: NSObjectProtocol? = nil
+    let notification = UIDevice.orientationDidChangeNotification
+    
+    init(updateInterval: TimeInterval) {
+        self.updateInterval = updateInterval
+    }
+    
+    func start() {
+        UIDevice.current.beginGeneratingDeviceOrientationNotifications()
+        orientationObserver = NotificationCenter.default.addObserver(forName: notification, object: nil, queue: .main, using: { [weak self] _ in
+            switch UIDevice.current.orientation {
+            case .faceUp, .faceDown, .unknown:
+                break
+            default:
+                self?.currentOrientation = UIDevice.current.orientation
+            }
+        })
+        
+        if motionManager.isDeviceMotionAvailable {
+            motionManager.startDeviceMotionUpdates()
+            
+            timer = Timer.scheduledTimer(withTimeInterval: updateInterval, repeats: true, block: { _ in
+            //MARK: TODO self.updateMotionData()
+            })
+        }
+    }
+}
